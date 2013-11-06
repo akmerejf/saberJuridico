67611ecc7e8b677d952d39af57ac3162fabdecc9
Felipe <felipe@fs-15.(none)>
2013-11-06 11:33:14 -0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 SaberJuridico::Application.routes.draw do
-  resources :volumes
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

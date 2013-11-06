3cd735f33aebf225f63a9d643a79aec89409d3cd
akmerejf <akmerejf@gmail.com>
2013-11-06 10:05:57 -0300

    teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaberJuridico::Application.config.secret_key_base = 'a785450a4140a39f3e852da5b971f4524c70216ba313f3956e29f26e86d2890842708aa37b5e1af19cd0e9b201062e51bcd08069c216d7efeb53c49cca08ba9b'

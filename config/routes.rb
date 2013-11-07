745bf8ed6a495574031f3229153d770b2d69b29c
akmerejf <akmerejf@gmail.com>
2013-11-07 00:15:44 -0300

    implementando o bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SaberJuridico::Application.routes.draw do
   resources :volumes
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'volumes#index'
+   root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106140008) do
+ActiveRecord::Schema.define(version: 20131107002839) do
 
   create_table "volumes", force: true do |t|
     t.string   "titulo"

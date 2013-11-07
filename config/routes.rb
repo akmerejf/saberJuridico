7c54b4b0739a81a6b12333277bc4462b19a0a3d6
Felipe <felipe@fs-15.(none)>
2013-11-07 09:02:56 -0300

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SaberJuridico::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :volumes
   # You can have the root of your site routed with "root"
    root 'volumes#index'
 
